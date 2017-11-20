c17ec98fbbad1ecd4cbb5fe00c3645a287bee8d0
Lauri Makkonen <laurimakkonen@hotmail.fi>
2017-11-20 16:48:07 +0000

    Korjailin koodia.
    git-svn-id: https://svn.cc.jyu.fi/srv/svn/ohj1-pelit/2017s/laanhema/trunk@22939 a045da7b-9728-470d-ae4b-0a4eae343f79

--- suunnitelma/TammoneTylsaOhjelma/TammoneTylsaOhjelma.cs
+++ suunnitelma/TammoneTylsaOhjelma/TammoneTylsaOhjelma.cs
@@ -1,71 +1,97 @@
 ﻿using System;
 using System.IO;
-using System.Text;
-using System.Linq;
 using System.Collections.Generic;
 
 /// @author Lauri Makkonen
-/// @version 26.10.2017
+/// @version 20.11.2017
 /// <summary>
 /// Ohjelma, mikä vertailee kahden kansion tiedostoja.
 /// </summary>
 public class TammoneTylsaOhjelma
+
 {
-    /// <summary>
-    /// 
-    /// </summary>
+    const string quote = "\"";
     public static void Main()
     {
-        Console.WriteLine("Write the path of the first folder:");
-        Console.Write(">");
-        string a = Console.ReadLine();
-        Directory.SetCurrentDirectory(a);
-        int totalfilesA = Directory.GetFiles(a, "*.*", SearchOption.AllDirectories).Length;
-        string[] listA = new string[totalfilesA];
-        string[] listANoPath = new string[totalfilesA];
-        listA = Directory.GetFiles(a, "*.*", SearchOption.AllDirectories);
-        for (int i = 0; i < listA.Length; i++)
+        string folder1 = "";
+        string[] a = Directory.GetLogicalDrives();
+        Console.WriteLine(a);
+        do
         {
-            listANoPath[i] = Path.GetFileName(listA[i]);
-            Console.WriteLine(listANoPath[i]);
+            Console.WriteLine("Write the path of the first folder:");
+            Console.WriteLine(@"Example " + quote + @"X:\XXXX\XXXX\XXXX\..." + quote);
+            Console.Write(">");
+            folder1 = Console.ReadLine();
+            if (!Directory.Exists(folder1) == true)
+                Console.WriteLine("Invalid path!");
+        }
+        while (!Directory.Exists(folder1) == true);
+        int folder1AmountOfFilesInside = Directory.GetFiles(folder1, "*.*", SearchOption.AllDirectories).Length;
+        string[] folder1Array = new string[folder1AmountOfFilesInside];
+        string[] folder1ArrayNoPath = new string[folder1AmountOfFilesInside];
+        folder1Array = Directory.GetFiles(folder1, "*.*", SearchOption.AllDirectories);
+
+        for (int i = 0; i < folder1ArrayNoPath.Length; i++)
+        {
+            folder1ArrayNoPath[i] = Path.GetFileName(folder1Array[i]);
+            Console.WriteLine(folder1ArrayNoPath[i]);
         }
         Console.WriteLine("");
-        Console.WriteLine("Contains " + totalfilesA + " files.");
-        Console.WriteLine("------------------------------------------------");
-        Console.WriteLine("Write the path of the second folder:");
-        Console.Write(">");
-        string b = (Console.ReadLine());
-        Directory.SetCurrentDirectory(b);
-        int totalfilesB = Directory.GetFiles(b, "*.*", SearchOption.AllDirectories).Length;
-        string[] listB = new string[totalfilesB];
-        listB = Directory.GetFiles(b, "*.*", SearchOption.AllDirectories);
-        string[] listBNoPath = new string[totalfilesB];
-        for (int i = 0; i < listB.Length; i++)
-        {
-            listBNoPath[i] = Path.GetFileName(listB[i]);
-            Console.WriteLine(listBNoPath[i]);
-        }
-        Console.WriteLine("");
-        Console.WriteLine("Contains " + totalfilesB + " files.");
+        Console.WriteLine("Contains " + folder1AmountOfFilesInside + " files.");
         Console.WriteLine("------------------------------------------------");
 
-        List<string> list1 = new List<string> {};
-        for (int i = 0; i < listANoPath.Length; i++)
+
+
+
+        string folder2 = "";
+        do
         {
-            list1.Add(listANoPath[i]);
+            Console.WriteLine("Write the path of the second folder:");
+            Console.WriteLine(@"Example " + quote + @"X:\XXXX\XXXX\XXXX\..." + quote);
+            Console.Write(">");
+            folder1 = Console.ReadLine();
+            if (!Directory.Exists(folder2) == true)
+                Console.WriteLine("Invalid path!");
+        }
+        while (!Directory.Exists(folder2) == true);
+        int folder2AmountOfFilesInside = Directory.GetFiles(folder2, "*.*", SearchOption.AllDirectories).Length;
+        string[] folder2Array = new string[folder2AmountOfFilesInside];
+        string[] folder2ArrayNoPath = new string[folder2AmountOfFilesInside];
+        folder2Array = Directory.GetFiles(folder1, "*.*", SearchOption.AllDirectories);
+
+        for (int i = 0; i < folder2ArrayNoPath.Length; i++)
+        {
+            folder2ArrayNoPath[i] = Path.GetFileName(folder2Array[i]);
+            Console.WriteLine(folder2ArrayNoPath[i]);
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Contains " + folder2AmountOfFilesInside + " files.");
+        Console.WriteLine("------------------------------------------------");
+
+
+
+
+
+
+
+
+
+        List<string> list1 = new List<string> {};
+        for (int i = 0; i < folder1ArrayNoPath.Length; i++)
+        {
+            list1.Add(folder1ArrayNoPath[i]);
         }
         List<string> list2 = new List<string> {};
-        for (int i = 0; i < listBNoPath.Length; i++)
+        for (int i = 0; i < folder2Array.Length; i++)
         {
-            list2.Add(listBNoPath[i]);
+            list2.Add(folder2Array[i]);
         }
-        int biggest = WhichIsBigger(totalfilesA, totalfilesB);
         List<string> missingfromA = new List<string> {};
         List<string> missingfromB = new List<string> {};
-        for (int i = 0; i < listANoPath.Length; i++)
+        for (int i = 0; i < folder1ArrayNoPath.Length; i++)
         {
             if
-            (list2.Contains(listANoPath[i]))
+            (list2.Contains(folder1ArrayNoPath[i]))
             { i++; }
             else
             {
@@ -73,7 +99,7 @@ public class TammoneTylsaOhjelma
                 i++;
             }
         }
-        for (int i = 0; i < listBNoPath.Length; i++)
+        for (int i = 0; i < folder2Array.Length; i++)
             {
             if
             (list1.Contains(list2[i]))
@@ -90,13 +116,7 @@ public class TammoneTylsaOhjelma
         File.WriteAllLines(c + @"\missingfromB.txt", missingfromB);
         File.WriteAllLines(c + @"\missingfromA.txt", missingfromA);
 
+        //Aliohjelma joka etsii folderin (tarvitaan sekä folder 1 sekä 2 mutta samat koodit)
 
-    }
-    public static int WhichIsBigger(int totalfilesA, int totalfilesB)
-    {
-        if (totalfilesA > totalfilesB)
-            return totalfilesA;
-        else
-            return totalfilesB;
     }
 }
